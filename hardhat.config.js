f568199cd508da5b56d473a106ff5bceee855e2a
gourav <gvirus21yt@gmail.com>
2022-02-08 18:30:30 +0530

    finished setting up hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,19 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+require("dotenv").config();
+
 require("@nomiclabs/hardhat-waffle");
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.11",
+  networks: {
+    ropsten: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    }
+  }
 };
+
